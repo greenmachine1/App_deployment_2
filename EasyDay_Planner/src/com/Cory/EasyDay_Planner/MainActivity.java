6c4b10839498b31b39d0e337d25fcd9b33d64234
greenmachine1 <scary_penguins05@hotmail.com>
2014-01-15 01:37:48 -0800

    added an onclick handler for the action bar

--- EasyDay_Planner/src/com/Cory/EasyDay_Planner/MainActivity.java
+++ EasyDay_Planner/src/com/Cory/EasyDay_Planner/MainActivity.java
@@ -4,7 +4,9 @@ import com.Cory.EasyDay_Planner.R;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -26,6 +28,31 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    
+    
+    
+    // Action bar selection handler
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+		
+    	switch (item.getItemId()){
+    	case R.id.add_event:
+    		Log.i("Action bar", "add button selected");
+    		return true;
+    	
+    	case R.id.search_overflow:
+    		Log.i("Action bar", "Search button selected");
+    		return true;
+    	case R.id.about_this_app_overflow:
+    		Log.i("Action bar", "about this app button selected");
+    		return true;
+    		
+    		default:
+    			return super.onOptionsItemSelected(item);
+    	}
+    	
     }
     
 }

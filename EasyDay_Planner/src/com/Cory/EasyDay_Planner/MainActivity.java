69440489a4f7f6128460215c94dd8a8089d4c21e
greenmachine1 <scary_penguins05@hotmail.com>
2014-01-15 03:24:05 -0800

    added the plus icon as well as working on my theme for the action bar

--- EasyDay_Planner/src/com/Cory/EasyDay_Planner/MainActivity.java
+++ EasyDay_Planner/src/com/Cory/EasyDay_Planner/MainActivity.java
@@ -4,12 +4,16 @@ import com.Cory.EasyDay_Planner.R;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.view.MenuItemCompat;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SearchView;
 
 public class MainActivity extends Activity {
 
+	
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,8 +31,15 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+
+        
         return true;
     }
+    
+    
+
+    
+    
     
     
     

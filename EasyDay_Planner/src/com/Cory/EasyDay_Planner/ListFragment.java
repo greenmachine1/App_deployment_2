c942b21ab89bb6c9a2fe83b7c073425508d0a85d
greenmachine1 <scary_penguins05@hotmail.com>
2014-01-19 01:05:33 -0800

    commit

--- EasyDay_Planner/src/com/Cory/EasyDay_Planner/ListFragment.java
+++ EasyDay_Planner/src/com/Cory/EasyDay_Planner/ListFragment.java
@@ -6,7 +6,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class ListFragment extends Fragment {
@@ -43,12 +42,13 @@ public class ListFragment extends Fragment {
 		view = inflater.inflate(R.layout.elements_list_fragment, container, false);
 		
 	
-		// this info is just temporary
-		String[] elements = {"First", "Second", "Third", "fourth", "fifth", "sixth", "seventh"};
+
 		
 		// Targeting the elements list
 		elementsListView = (ListView)view.findViewById(R.id.elements_list);
 		
+		
+		// temporary arrays
 		String[] firstRowElements = {"Grant Break", "Take a break at 12:15pm", "Do Laundry by 5pm", "Take Fido out for a walk", "Pick up Son at 7pm" };
 		String[] secondRowElements = {"13:25 Left", "Alarm set for 10 minutes prior", "Alarm set for 1 hour prior", "At some point walk the dog", "Alarm set for 10 minutes prior"};
 		

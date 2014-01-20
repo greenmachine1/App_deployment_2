7ede7c4517180a762fa6100dd0b2810c7e98a6d7
greenmachine1 <scary_penguins05@hotmail.com>
2014-01-20 02:19:47 -0800

    more work on my spinner and converting things to json data

--- EasyDay_Planner/src/com/Cory/EasyDay_Planner/New_Event.java
+++ EasyDay_Planner/src/com/Cory/EasyDay_Planner/New_Event.java
@@ -3,31 +3,51 @@ package com.Cory.EasyDay_Planner;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
-public class New_Event extends Activity{
+public class New_Event extends Activity implements OnItemSelectedListener{
 
 	
 	Spinner categorySpinner;
 	Button setAnAlarmButton;
 	Button setATimeButton;
 	
+	EditText nameOfEvent;
+	EditText noteText;
+	
+	String selectedItemFromCategory;
+	
+	Context _context;
+
+
+	
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
     	 super.onCreate(savedInstanceState);
     	 
+    	 _context = this;
+    	 
     	 // calling on my list view fragment
          setContentView(R.layout.new_event_layout);
          
-         setAnAlarmButton = (Button)findViewById(R.id.alarm_set);
-         setATimeButton = (Button)findViewById(R.id.time_set);
+         // targeting my buttons
+         setAnAlarmButton = (Button)findViewById(R.id.alarm_set_button);
+         setATimeButton = (Button)findViewById(R.id.time_set_button);
+         
+         nameOfEvent = (EditText)findViewById(R.id.event_name);
+         noteText = (EditText)findViewById(R.id.event_note);
          
          
          // setting the alarm button click listener
@@ -56,29 +76,56 @@ public class New_Event extends Activity{
         	 
          });
          
-
+         
+         
          // set category info
-         categorySpinner();
+         categorySpinner = (Spinner)findViewById(R.id.category_spinner);
+         
+         // creation of the array adapter for category dropdown
+         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, 
+        		 R.array.category_array_list, 
+        		 android.R.layout.simple_spinner_dropdown_item);
+         
+         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+         
+         categorySpinner.setAdapter(adapter);
+         
+         categorySpinner.setOnItemSelectedListener(this);
+         
          
 	}
 	
 	
 	
-	// category spinner
-	public void categorySpinner(){
+
+	
+	
+	
+	
+	
+	// when the user selects a drop down item
+	// this is what happens
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View view, int position,
+			long id) {
 		
+		String[] categoryArray = {"Home","Home", "Work", "School", "Other"};
 		
-		categorySpinner = (Spinner)findViewById(R.id.category_spinner);
-        
-        // creation of the array adapter for category dropdown
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, 
-       		 R.array.category_array_list, 
-       		 android.R.layout.simple_spinner_dropdown_item);
-        
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        
-        categorySpinner.setAdapter(adapter);
-        
+		selectedItemFromCategory = categoryArray[position].toString();
+		
+		Log.i("category", selectedItemFromCategory);
+		
+	}
+
+
+
+	@Override
+	public void onNothingSelected(AdapterView<?> parent) {
+		// TODO Auto-generated method stub
+		
+		// the default category
+		selectedItemFromCategory = "Home";
+		
 	}
 	
 	
@@ -99,14 +146,38 @@ public class New_Event extends Activity{
 	
 	
 	
+	
+	// Action bar selection handler
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+		
+    	switch (item.getItemId()){
+    	
+    	// what to do when the add event icon is selected 
+    	case R.id.done_icon:
+    		
+    		// writing everything to json format
+    		writeJson();
+			
+    		return true;
+
+    		default:
+    			return super.onOptionsItemSelected(item);
+    	}
+    	
+    }
+	
+	
+	
+	// writing the json data
 	public void writeJson(){
 		JSONObject jsonObject = new JSONObject();
 		
 		try{
 			
-			jsonObject.put("name", "My name is Cory");
-			jsonObject.put("age", "29");
-			jsonObject.put("category", "home");
+			jsonObject.put("name_of_event", nameOfEvent.getText().toString());
+			jsonObject.put("note_for_event", noteText.getText().toString());
+			jsonObject.put("category", selectedItemFromCategory.toString());
 			
 		}catch(Exception e){
 			//e.printStackTrace();
@@ -115,4 +186,12 @@ public class New_Event extends Activity{
 		
 		Log.i("object contains", jsonObject.toString());
 	}
+
+
+
+
+	
+	
+	
+	
 }
